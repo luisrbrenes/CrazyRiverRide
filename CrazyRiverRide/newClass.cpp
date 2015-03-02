87744eab5d08284893691ce7110f1d98c678e49b
luisrbrenes <luisrbrenes@luisrbrenes>
2015-03-01 21:14:51 -0600

    prueba de luis

---
+++ CrazyRiverRide/newClass.cpp
@@ -0,0 +1,24 @@
+/* 
+ * File:   newClass.cpp
+ * Author: luisrbrenes
+ * 
+ * Created on March 1, 2015, 9:13 PM
+ */
+
+
+
+//Prueba de Git
+
+
+
+#include "newClass.h"
+
+newClass::newClass() {
+}
+
+newClass::newClass(const newClass& orig) {
+}
+
+newClass::~newClass() {
+}
+
